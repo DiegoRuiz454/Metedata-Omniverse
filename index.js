b1ccd0a1811aae51b9de9d55e9b0d6a502047db7
DiegoRuiz454 <differ454@hotmail.com>
2022-05-26 18:32:51 +0200

    rfgt

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const data = {
 
 jsonfile.writeFile(FILE_PATH, data);
 
-simpleGit().add([FILE_PATH]).commit(DATE, {"--date": DATE});
+simpleGit().add([FILE_PATH]).commit(DATE, {"--date": DATE}).push();
 
 
 
